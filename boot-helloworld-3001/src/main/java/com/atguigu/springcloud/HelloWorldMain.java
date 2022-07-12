7126c04e41c686a22db11f2287eaef6af8476305
linbicheng <linbicheng@travelsky.com>
2022-07-12 09:06:44 +0800

    生产者8001

--- boot-helloworld-3001/src/main/java/com/atguigu/springcloud/HelloWorldMain.java
+++ boot-helloworld-3001/src/main/java/com/atguigu/springcloud/HelloWorldMain.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldMain {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldMain.class,args);
-        ==================
     }
 }
